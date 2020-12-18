293a5303c069b18d240cae39ae5660caff31a835
Scala Steward <me@scala-steward.org>
2020-12-18 20:34:56 +0100

    Update cats-core, cats-effect to 2.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val cats       = "2.3.0"
-    val catsEffect = "2.3.0"
+    val cats       = "2.3.1"
+    val catsEffect = "2.3.1"
     val fs2        = "2.4.6"
     val http4s     = "0.21.14"
     val jwt        = "4.3.0"
