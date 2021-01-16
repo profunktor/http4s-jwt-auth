35b6d5e5ec32825e7c60cdf75ff70ee3c9762e66
Scala Steward <me@scala-steward.org>
2021-01-16 11:22:10 +0100

    Update kind-projector to 0.11.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val jwt        = "4.3.0"
 
     val betterMonadicFor = "0.3.1"
-    val kindProjector    = "0.11.2"
+    val kindProjector    = "0.11.3"
 
     val scalaTest = "3.2.3"
   }
