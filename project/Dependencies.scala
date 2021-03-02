ad7f942d658436094d84536adc4a0a4061dbf4b7
Scala Steward <me@scala-steward.org>
2021-03-02 19:09:11 +0100

    Update fs2-core to 2.5.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val cats       = "2.4.2"
     val catsEffect = "2.3.3"
-    val fs2        = "2.5.2"
+    val fs2        = "2.5.3"
     val http4s     = "0.21.20"
     val jwt        = "5.0.0"
 
