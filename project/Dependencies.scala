edff647b5400ed048c2ce063c32beea36b47c0c4
Scala Steward <me@scala-steward.org>
2021-09-18 08:04:01 +0200

    Update cats-effect to 3.2.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object V {
     val cats       = "2.6.1"
-    val catsEffect = "3.2.8"
+    val catsEffect = "3.2.9"
     val fs2        = "3.1.2"
     val http4s     = "0.23.3"
     val jwt        = "9.0.1"
