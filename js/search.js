3cfd8bc76b926d42b4e6a843f8842082e66f2401
Frederick Roth <frederick.roth@breuninger.de>
2024-11-04 10:51:10 +0100

    updated site

---
+++ js/search.js
@@ -0,0 +1,195 @@
+// When the user clicks on the search box, we want to toggle the search dropdown
+function displayToggleSearch(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  closeDropdownSearch(e);
+  
+  if (idx === null) {
+    console.log("Building search index...");
+    prepareIdxAndDocMap();
+    console.log("Search index built.");
+  }
+  const dropdown = document.querySelector("#search-dropdown-content");
+  if (dropdown) {
+    if (!dropdown.classList.contains("show")) {
+      dropdown.classList.add("show");
+    }
+    document.addEventListener("click", closeDropdownSearch);
+    document.addEventListener("keydown", searchOnKeyDown);
+    document.addEventListener("keyup", searchOnKeyUp);
+  }
+}
+
+//We want to prepare the index only after clicking the search bar
+var idx = null
+const docMap = new Map()
+
+function prepareIdxAndDocMap() {
+  const docs = [  
+    {
+      "title": "Code of Conduct",
+      "url": "/CODE_OF_CONDUCT.html",
+      "content": "Code of Conduct We are committed to providing a friendly, safe and welcoming environment for all, regardless of level of experience, gender, gender identity and expression, sexual orientation, disability, personal appearance, body size, race, ethnicity, age, religion, nationality, or other such characteristics. Everyone is expected to follow the Scala Code of Conduct when discussing the project on the available communication channels. If you are being harassed, please contact us immediately so that we can support you. ## Moderation For any questions, concerns, or moderation requests please contact a member of the project."
+    } ,    
+    {
+      "title": "Home",
+      "url": "/",
+      "content": "http4s-jwt-auth Opinionated JWT authentication library for http4s. Dependencies jwt-scala is being used to encode and decode JWT tokens. Add the following dependency to your build.sbt (check latest version on the badge): \"dev.profunktor\" %% \"http4s-jwt-auth\" % Version This library currently only supports the stable 0.23 series of http4s. Usage import cats.effect.IO import cats.implicits._ import dev.profunktor.auth._ import dev.profunktor.auth.jwt._ import pdi.jwt._ import org.http4s._ case class AuthUser(id: Long, name: String) // i.e. retrieve user from database val authenticate: JwtToken =&gt; JwtClaim =&gt; IO[Option[AuthUser]] = token =&gt; claim =&gt; AuthUser(123L, \"joe\").some.pure[IO] val jwtAuth = JwtAuth.hmac(Array('5','3','c','r','3','t'), JwtAlgorithm.HS256) val middleware = JwtAuthMiddleware[IO, AuthUser](jwtAuth, authenticate) val routes: AuthedRoutes[AuthUser, IO] = ??? val securedRoutes: HttpRoutes[IO] = middleware(routes) Build microsite You need to have jekyll installed before running the following command: $ sbt makeMicrosite $ cd site/target/site $ jekyll serve Or you can use nix develop by leveraging the flake.nix in this repo. Notes This library is quite opinionated, use with caution. Examples and docs coming soon! If you would like to see support for any other functionality come have a chat in the Gitter channel!"
+    } ,        
+  ];
+
+  idx = lunr(function () {
+    this.ref("title");
+    this.field("content");
+
+    docs.forEach(function (doc) {
+      this.add(doc);
+    }, this);
+  });
+
+  docs.forEach(function (doc) {
+    docMap.set(doc.title, doc.url);
+  });
+}
+
+// The onkeypress handler for search functionality
+function searchOnKeyDown(e) {
+  const keyCode = e.keyCode;
+  const parent = e.target.parentElement;
+  const isSearchBar = e.target.id === "search-bar";
+  const isSearchResult = parent ? parent.id.startsWith("result-") : false;
+  const isSearchBarOrResult = isSearchBar || isSearchResult;
+
+  if (keyCode === 40 && isSearchBarOrResult) {
+    // On 'down', try to navigate down the search results
+    e.preventDefault();
+    e.stopPropagation();
+    selectDown(e);
+  } else if (keyCode === 38 && isSearchBarOrResult) {
+    // On 'up', try to navigate up the search results
+    e.preventDefault();
+    e.stopPropagation();
+    selectUp(e);
+  } else if (keyCode === 27 && isSearchBarOrResult) {
+    // On 'ESC', close the search dropdown
+    e.preventDefault();
+    e.stopPropagation();
+    closeDropdownSearch(e);
+  }
+}
+
+// Search is only done on key-up so that the search terms are properly propagated
+function searchOnKeyUp(e) {
+  // Filter out up, down, esc keys
+  const keyCode = e.keyCode;
+  const cannotBe = [40, 38, 27];
+  const isSearchBar = e.target.id === "search-bar";
+  const keyIsNotWrong = !cannotBe.includes(keyCode);
+  if (isSearchBar && keyIsNotWrong) {
+    // Try to run a search
+    runSearch(e);
+  }
+}
+
+// Move the cursor up the search list
+function selectUp(e) {
+  if (e.target.parentElement.id.startsWith("result-")) {
+    const index = parseInt(e.target.parentElement.id.substring(7));
+    if (!isNaN(index) && (index > 0)) {
+      const nextIndexStr = "result-" + (index - 1);
+      const querySel = "li[id$='" + nextIndexStr + "'";
+      const nextResult = document.querySelector(querySel);
+      if (nextResult) {
+        nextResult.firstChild.focus();
+      }
+    }
+  }
+}
+
+// Move the cursor down the search list
+function selectDown(e) {
+  if (e.target.id === "search-bar") {
+    const firstResult = document.querySelector("li[id$='result-0']");
+    if (firstResult) {
+      firstResult.firstChild.focus();
+    }
+  } else if (e.target.parentElement.id.startsWith("result-")) {
+    const index = parseInt(e.target.parentElement.id.substring(7));
+    if (!isNaN(index)) {
+      const nextIndexStr = "result-" + (index + 1);
+      const querySel = "li[id$='" + nextIndexStr + "'";
+      const nextResult = document.querySelector(querySel);
+      if (nextResult) {
+        nextResult.firstChild.focus();
+      }
+    }
+  }
+}
+
+// Search for whatever the user has typed so far
+function runSearch(e) {
+  if (e.target.value === "") {
+    // On empty string, remove all search results
+    // Otherwise this may show all results as everything is a "match"
+    applySearchResults([]);
+  } else {
+    const tokens = e.target.value.split(" ");
+    const moddedTokens = tokens.map(function (token) {
+      // "*" + token + "*"
+      return token;
+    })
+    const searchTerm = moddedTokens.join(" ");
+    const searchResults = idx.search(searchTerm);
+    const mapResults = searchResults.map(function (result) {
+      const resultUrl = docMap.get(result.ref);
+      return { name: result.ref, url: resultUrl };
+    })
+
+    applySearchResults(mapResults);
+  }
+
+}
+
+// After a search, modify the search dropdown to contain the search results
+function applySearchResults(results) {
+  const dropdown = document.querySelector("div[id$='search-dropdown'] > .dropdown-content.show");
+  if (dropdown) {
+    //Remove each child
+    while (dropdown.firstChild) {
+      dropdown.removeChild(dropdown.firstChild);
+    }
+
+    //Add each result as an element in the list
+    results.forEach(function (result, i) {
+      const elem = document.createElement("li");
+      elem.setAttribute("class", "dropdown-item");
+      elem.setAttribute("id", "result-" + i);
+
+      const elemLink = document.createElement("a");
+      elemLink.setAttribute("title", result.name);
+      elemLink.setAttribute("href", result.url);
+      elemLink.setAttribute("class", "dropdown-item-link");
+
+      const elemLinkText = document.createElement("span");
+      elemLinkText.setAttribute("class", "dropdown-item-link-text");
+      elemLinkText.innerHTML = result.name;
+
+      elemLink.appendChild(elemLinkText);
+      elem.appendChild(elemLink);
+      dropdown.appendChild(elem);
+    });
+  }
+}
+
+// Close the dropdown if the user clicks (only) outside of it
+function closeDropdownSearch(e) {
+  // Check if where we're clicking is the search dropdown
+  if (e.target.id !== "search-bar") {
+    const dropdown = document.querySelector("div[id$='search-dropdown'] > .dropdown-content.show");
+    if (dropdown) {
+      dropdown.classList.remove("show");
+      document.documentElement.removeEventListener("click", closeDropdownSearch);
+    }
+  }
+}
