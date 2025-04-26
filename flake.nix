5f6042634fd92dd17996ce2e758b490fe1b99707
Gabriel Volpe <volpegabriel@gmail.com>
2025-04-26 13:54:37 +0200

    nix: dev shell update (#513)

--- flake.nix
+++ flake.nix
@@ -2,21 +2,28 @@
   description = "Scala development shell";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        jreOverlay = f: p: {
-          jre = p.jdk19;
-        };
-
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ jreOverlay ];
-        };
+        overlays = [
+          (self: super: {
+            jre = super.jdk21_headless;
+            sbt = super.sbt.overrideAttrs (
+              old: {
+                nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [ super.makeWrapper ];
+                # Setting SBT_OPTS because of this bug: https://github.com/sbt/sbt-site/issues/169
+                postInstall = ''
+                  wrapProgram $out/bin/sbt --suffix SBT_OPTS : '--add-opens java.base/java.lang=ALL-UNNAMED'
+                '';
+              }
+            );
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; };
       in
       {
         devShell = pkgs.mkShell {
@@ -29,8 +36,13 @@
             sbt
           ];
 
-          JAVA_HOME = "${pkgs.jre}";
+          shellHook = ''
+            JAVA_HOME="${pkgs.jre}"
+          '';
         };
-      }
-    );
+
+        packages = {
+          inherit (pkgs) sbt;
+        };
+      });
 }
